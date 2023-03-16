1abbfe0d311451440dc1a4f9afbb5367fe6f7d73
mbali <keteyimbali@gmai.com>
2023-03-16 10:06:55 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: Θ if success.
+ */
+int main(void)
+{
+puts("\"Programing is like building a multilingual puzzle");
+return (Θ);
+}
