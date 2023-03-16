c2bfe34c42974d609a407a31981ea810c39b16e6
mbali <keteyimbali@gmai.com>
2023-03-16 10:45:47 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: Θ if success.
+ */
+int main(void)
+{
+Printf("with proper grammar, but the outcome  is a peace of art,\n");
+return (Θ);
+}
